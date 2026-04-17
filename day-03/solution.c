0a52de404bf0ed9632b89360599eb3cafff62492
P Manorenjitha <manorenjitha2006@gmail.com>
2026-04-17 20:28:50 +0530

    Create solution.c
    Day3

---
+++ day-03/solution.c
@@ -0,0 +1,26 @@
+//MOVE ZEROES TO THE END 
+// TWO POINTER TECHNIQUE 
+#include<stdio.h>
+#define SIZE 35
+int main(){
+int N,arr[SIZE],temp;
+scanf("%d",&N);
+int j=0;
+for(int i=0;i<N;i++)
+    scanf("%d",&arr[i]);
+  
+for(int i=0;i<N;i++){
+   if(arr[i]!=0){
+    temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    j++;
+
+   }
+  
+}
+
+for(int i=0;i<N;i++)
+printf("%d ",arr[i]);
+ return 0;
+}
